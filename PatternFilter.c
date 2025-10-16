c18d0164e774478180402eda53a16078bc168e3f
Dhairya Marwaha <ardh.marwaha@gmail.com>
2025-10-16 10:02:03 +0530

    Update PatternFilter.c

--- PatternFilter.c
+++ PatternFilter.c
@@ -1,84 +1,99 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include<stdbool.h>
+
 void patternFilter(int array[], int size);
-bool palindromeOperator(int num);
-bool oddOperation(int num);
+void arrayPrinter(int array[], int size);
+bool isPalindrome(int num);
+bool isOdd(int num);
+bool isDivisible3(int num);
+
+int digitsSum(int num);
+int digitsCount(int num);
+
 int main(){
     int size;
+    printf("Enter the size of the array : ");
     scanf("%d", &size);
 
     int array[size];
 
     for(int i = 0; i < size; i++){
+        printf("Enter the %d element : ", (i+1));
         scanf("%d", &array[i]);
     }
 
     patternFilter(array, size);
+    arrayPrinter(array, size);
+
 }
 
-void patternFilter(int array[], int size)
+void patternFilter(int array[], int size){
+    for(int i = 0; i < size; i++){
+
+        bool condition1 = isPalindrome(array[i]);
+        bool condition2a = isOdd(array[i]);
+        bool condition2b = isDivisible3(array[i]);
+
+        if(condition1){
+            array[i] = digitsSum(array[i]);
+        } else if (condition2a == false && condition2b  )
+        {
+            array[i] = digitsCount(array[i]);
+        }
+        
+    }
+}
+
+void arrayPrinter(int array[], int size)
 {
     for(int i = 0; i < size; i++){
-        bool palindromeOperation = palindromeOperator(array[i]);
-        bool secondOperation = oddOperation(array[i]);
-        if(palindromeOperation){
-            int sumOfDigits = 0;
-            int num = array[i];
-            while (num > 0){
-                int digits = num % 10;
-                sumOfDigits += digits;
-                num /= 10;
-            }
-            array[i] = sumOfDigits;
-        } else if (secondOperation)
-        {
-            int digitCount = 0;
-            int number = array[i];
-            while(number > 0){
-                digitCount++;
-                number /= 10;
-            }
-            array[i] = digitCount;
-        } else{
-            array[i] = array[i];
-        }
+        printf("%d ", array[i]);
     }
-     for (int i = 0; i < size; i++){
-            printf("%d ", array[i]);
-        }
 }
 
-bool palindromeOperator(int num)
+bool isPalindrome(int num)
 {
+    int ogNum = num;
     int revNum = 0;
     bool palindrome = false;
-    int ogNum = num;
-    int realNumber = num;
-    int sumOfDigits = 0;
+    
     while(num > 0){
         int digits = num % 10;
         revNum = revNum * 10 + digits;
         num /= 10;
     }
-    if(revNum != ogNum){
-        palindrome = false;
-    } else{
+
+    if(revNum == ogNum){
         palindrome = true;
     }
     return palindrome;
 }
 
-bool oddOperation(int num)
-{
-    bool isOdd = false;
-    bool condition = false;
-    int realNum = num;
-    if(num % 2 == 0){
-        isOdd = false;
-    }else{
-        if(num % 3 == 0){
-            condition = true;
-        }
+int digitsSum(int num){
+    int sum = 0;
+    while(num > 0){
+        int digits = num % 10; 
+        sum += digits;
+        num /= 10;
     }
-    return condition;
+    return sum;
+}
+
+int digitsCount(int num)
+{
+    int digits = 0;
+    while(num > 0){
+        digits++;
+        num /= 10;
+    }
+    return digits;
+}
+
+bool isOdd(int num){
+    return (num % 2 == 0);
+}
+
+bool isDivisible3(int num)
+{
+    return (num % 3 == 0);
 }
